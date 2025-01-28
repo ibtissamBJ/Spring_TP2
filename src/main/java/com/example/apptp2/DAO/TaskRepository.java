923fa22a12e357ba027436bcea77b920075c02d0
ibtissamBJ <161085061+ibtissamBJ@users.noreply.github.com>
2025-01-28 17:05:30 +0100

    Update TaskRepository.java
    add comment

--- src/main/java/com/example/apptp2/DAO/TaskRepository.java
+++ src/main/java/com/example/apptp2/DAO/TaskRepository.java
@@ -4,4 +4,5 @@ import com.example.apptp2.modele.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+  //add comment
 }
